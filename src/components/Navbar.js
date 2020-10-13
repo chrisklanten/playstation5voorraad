9897c102516b621e6dcb822dad166dc0aff81fe4
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2020-10-13 19:59:25 +0200

    add pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+    };
   }
 
   toggleHamburger = () => {
@@ -23,76 +23,70 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: "",
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
+      <>
+        <div className="w-full">
+          <Link to="/" className="py-10 block" title="Logo">
+            <img
+              src={logo}
+              alt="Playstation 5 voorraad"
+              className="w-1/2 mx-auto"
+            />
+          </Link>
+          {/* Hamburger menu */}
           <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+            data-target="navMenu"
+            onClick={() => this.toggleHamburger()}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+            <span />
+            <span />
+            <span />
           </div>
         </div>
-      </nav>
-    )
+        <nav
+          className="navbar is-transparent"
+          role="navigation"
+          aria-label="main-navigation"
+        >
+          <div className="container">
+            <div
+              id="navMenu"
+              className={`navbar-menu ${this.state.navBarActiveClass}`}
+            >
+              <div className="navbar-start has-text-centered mx-auto justify-center">
+                <Link className="navbar-item" to="/about">
+                  About
+                </Link>
+                <Link className="navbar-item" to="/products">
+                  Products
+                </Link>
+                <Link className="navbar-item" to="/blog">
+                  Blog
+                </Link>
+                <Link className="navbar-item" to="/contact">
+                  Contact
+                </Link>
+                <Link className="navbar-item" to="/contact/examples">
+                  Form Examples
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
+
+import "../components/style.scss";
 
 export const IndexPageTemplate = ({
   image,
@@ -16,103 +18,139 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
+    <div className="container flex flex-col justify-center items-center">
+      <h1>{title}</h1>
+      <h2>{subheading}</h2>
     </div>
     <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
+      <div className="container flex">
+        <div className="mr-16 w-full md:w-3/4">
+          <div className="">
+            <h3 className="title">{mainpitch.title}</h3>
+            <p className="subtitle">{mainpitch.description}</p>
+          </div>
+          <div className="mt-8">
+            <div className="container">
+              <div className="">
+                <div className="mr-8 flex">
+                  <p className="mb-4 font-bold text-center pb-4 border-b-2">
+                    Verkoper
+                  </p>
+
+                  <p className="mb-4 font-bold text-center pb-4 border-b-2">
+                    Pre-order
+                  </p>
+
+                  <p className="mb-4 font-bold text-center pb-4 border-b-2">
+                    Prijs
+                  </p>
+
+                  <p className="mb-4 font-bold text-center pb-4 border-b-2">
+                    Informatie pagina
+                  </p>
+                </div>
+                <div className="flex">
+                  <div>
+                    <img
+                      className="w-24 mb-4 mx-auto"
+                      src="https://cdn.freebiesupply.com/logos/thumbs/2x/coolblue-logo.png"
+                      alt="Pre order Playstation 5 bij Coolblue"
+                    />
                   </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
+                  <div>500,-</div>
+                  <div>
+                    <a
+                      className="w-24 mb-4 no-underline hover:underline"
+                      href="https://www.coolblue.nl/product/865866/playstation-5.html"
+                      title="Pre order Playstation 5 bij Coolblue"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Pre order bij Coolblue
+                    </a>
+                  </div>
+
+                  <div>
+                    <a
+                      className="w-24 mb-4 no-underline hover:underline"
+                      href="https://www.coolblue.nl/advies/playstation-5.html"
+                      title="Pre order Playstation 5 bij Coolblue"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Pre order informatie van Coolblue
+                    </a>
                   </div>
                 </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
+
+                <div className="flex">
+                  <div>
+                    <img
+                      className="w-32 mb-4"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Bol.com_logo.svg/1280px-Bol.com_logo.svg.png"
+                      alt="Pre order Playstation 5 bij Bol.com"
+                    />
+                  </div>
+                  <div>500,-</div>
+                  <div>
+                    <a
+                      className="w-24 mb-4 no-underline hover:underline"
+                      href="https://partner.bol.com/click/click?p=2&t=url&s=1073470&f=TXL&url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fp%2Fsony-playstation-5-console%2F9300000004162282%2F&name=Sony%20PlayStation%205%20Console"
+                      title="Pre order Playstation 5 bij Coolblue"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Pre order bij Bol.com
+                    </a>
+                  </div>
+
+                  <div>
+                    <a
+                      className="w-24 mb-4 no-underline hover:underline"
+                      href="https://partner.bol.com/click/click?p=2&t=url&s=1073470&f=TXL&url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fi%2Fwat-is-de-nieuwe-playstation-5%2FN%2F38910%2F&name=De%20nieuwe%20Playstation%205"
+                      title="Pre order Playstation 5 bij Coolblue"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Pre order informatie van Bol.com
+                    </a>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
+
+                <img
+                  className="w-32 mb-4"
+                  src="https://blackfridaydiscount.nl/wp-content/uploads/2016/12/Mediamarkt-2.png"
+                  alt="Pre order Playstation 5 bij Mediamarkt"
+                />
+
+                <img
+                  className="w-32 mb-4"
+                  src="https://s3-eu-west-1.amazonaws.com/tpd/logos/4cc8765500006400050e0e36/0x0.png"
+                  alt="Pre order Playstation 5 bij Gamemania"
+                />
+
+                <img
+                  className="w-32 mb-4"
+                  src="https://www.nedgame.nl/gfx/nedgame-logo.png"
+                  alt="Pre order Playstation 5 bij Nedgame"
+                />
+
+                <img
+                  className="w-32 mb-4"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/1200px-Amazon.com-Logo.svg.png"
+                  alt="Pre order Playstation 5 bij Amazon"
+                />
               </div>
             </div>
           </div>
         </div>
+        <div id="sidebar" className="w-full md:w-1/4">
+          {new Date().getFullYear()}
+        </div>
       </div>
     </section>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -124,10 +162,10 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,8 +179,8 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,9 +188,9 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +228,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
